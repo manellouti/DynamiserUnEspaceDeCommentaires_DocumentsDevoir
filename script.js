21ff379f6f19588840fff7ba3345b2b85cb12eb2
Manel <manellouti@live.fr>
2024-08-22 13:45:45 +0200

    Création de l'élément commentaire

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             errorMessage.style.display = 'none';
         }
+
+        // Création du nouvel élément commentaire
+        const newComment = document.createElement('div');
+        newComment.className = 'flex space-x-4 text-sm text-gray-500';
+        newComment.innerHTML = `
+            <div class="flex-1 py-10 border-t border-gray-200">
+                <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
+                <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                    <p>${message}</p>
+                </div>
+            </div>
+        `;
